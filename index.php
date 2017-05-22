fdd1710b39382836b6d7a8b924c0ecd05e436a89
timsalch <timsalch@outlook.com>
2017-05-22 08:23:34 -0700

    Update index.php

--- index.php
+++ index.php
@@ -2,4 +2,32 @@
 
 echo "Hello World!";
 
+//Testing facebook graph access
+
+//i dont think i need this section
+/*
+$fb = new Facebook\Facebook([
+  'app_id' => '{app-id}',
+  'app_secret' => '{app-secret}',
+  'default_graph_version' => 'v2.2',
+  ]);
+*/
+
+try {
+  // Returns a `Facebook\FacebookResponse` object
+  $response = $fb->get('/me?fields=id,name', '{access-token}');
+} catch(Facebook\Exceptions\FacebookResponseException $e) {
+  echo 'Graph returned an error: ' . $e->getMessage();
+  exit;
+} catch(Facebook\Exceptions\FacebookSDKException $e) {
+  echo 'Facebook SDK returned an error: ' . $e->getMessage();
+  exit;
+}
+
+$user = $response->getGraphUser();
+
+echo 'Name: ' . $user['name'];
+// OR
+// echo 'Name: ' . $user->getName();
+
 ?>
