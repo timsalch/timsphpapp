864a25237609869665bba8ba9367d99cdc5092f2
timsalch <timsalch@outlook.com>
2017-05-23 20:40:44 -0700

    Update index.php

--- index.php
+++ index.php
@@ -13,6 +13,33 @@ $fb = new Facebook\Facebook([
   ]);
 
 
+$helper = $fb->getCanvasHelper();
+
+try {
+  $accessToken = $helper->getAccessToken();
+} catch(Facebook\Exceptions\FacebookResponseException $e) {
+  // When Graph returns an error
+  echo 'Graph returned an error: ' . $e->getMessage();
+  exit;
+} catch(Facebook\Exceptions\FacebookSDKException $e) {
+  // When validation fails or other local issues
+  echo 'Facebook SDK returned an error: ' . $e->getMessage();
+  exit;
+}
+
+if (! isset($accessToken)) {
+  echo 'No OAuth data could be obtained from the signed request. User has not authorized your app yet.';
+  exit;
+}
+
+// Logged in
+echo '<h3>Signed Request</h3>';
+var_dump($helper->getSignedRequest());
+
+echo '<h3>Access Token</h3>';
+var_dump($accessToken->getValue());
+
+/*
 try {
   // Returns a `Facebook\FacebookResponse` object
   $response = $fb->get('/me?fields=id,name', '{access-token}');
@@ -29,5 +56,6 @@ $user = $response->getGraphUser();
 echo 'Name: ' . $user['name'];
 // OR
 // echo 'Name: ' . $user->getName();
+*/
 
 ?>
