2191ff52dc142efa071d5bd0871640df3a49e666
timsalch <timsalch@outlook.com>
2017-05-25 06:59:23 -0700

    Update index.php

--- index.php
+++ index.php
@@ -1,35 +1,44 @@
 <body>
 
-  
-<script>
-  window.fbAsyncInit = function() {
-    FB.init({
-      appId      : '111385752712817',
-      cookie     : true,
-      xfbml      : true,
-      version    : 'v2.8'
-    });
-    FB.AppEvents.logPageView();   
-  };
-
-  (function(d, s, id){
-     var js, fjs = d.getElementsByTagName(s)[0];
-     if (d.getElementById(id)) {return;}
-     js = d.createElement(s); js.id = id;
-     js.src = "//connect.facebook.net/en_US/sdk.js";
-     fjs.parentNode.insertBefore(js, fjs);
-   }(document, 'script', 'facebook-jssdk'));
-
-</script>
 
 <?php
+  $fb = new \Facebook\Facebook([
+  'app_id' => '{app-id}',
+  'app_secret' => '{app-secret}',
+  'default_graph_version' => 'v2.9',
+  //'default_access_token' => '{access-token}', // optional
+]);
 
+// Use one of the helper classes to get a Facebook\Authentication\AccessToken entity.
+//   $helper = $fb->getRedirectLoginHelper();
+//   $helper = $fb->getJavaScriptHelper();
+//   $helper = $fb->getCanvasHelper();
+//   $helper = $fb->getPageTabHelper();
+
+try {
+  // Get the \Facebook\GraphNodes\GraphUser object for the current user.
+  // If you provided a 'default_access_token', the '{access-token}' is optional.
+  $response = $fb->get('/me', '{access-token}');
+} catch(\Facebook\Exceptions\FacebookResponseException $e) {
+  // When Graph returns an error
+  echo 'Graph returned an error: ' . $e->getMessage();
+  exit;
+} catch(\Facebook\Exceptions\FacebookSDKException $e) {
+  // When validation fails or other local issues
+  echo 'Facebook SDK returned an error: ' . $e->getMessage();
+  exit;
+}
+
+$me = $response->getGraphUser();
+echo 'Logged in as ' . $me->getName();
+  
+/*
 echo "Hello World!";
 
 //Testing facebook graph access
 
 //i dont think i need this section
-/*/
+
 $fb = new Facebook\Facebook([
   'app_id' => '111385752712817',
   'app_secret' => 'b9e480d50582ea442b2308af63a50f15',
@@ -62,8 +71,7 @@ var_dump($helper->getSignedRequest());
 
 echo '<h3>Access Token</h3>';
 var_dump($accessToken->getValue());
-*/
-/*
+
 try {
   // Returns a `Facebook\FacebookResponse` object
   $response = $fb->get('/me?fields=id,name', '{access-token}');
@@ -74,13 +82,13 @@ try {
   echo 'Facebook SDK returned an error: ' . $e->getMessage();
   exit;
 }
-*/
+
 $user = $response->getGraphUser();
 
 echo 'Name: ' . $user['name'];
 // OR
 // echo 'Name: ' . $user->getName();
 
-
+*/
 ?>
 </body>
